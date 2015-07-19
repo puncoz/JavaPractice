b29bbcb75858abb95b4f7e5b23760bc8f2b6168e
Puncoz Nepal <puncoz138@gmail.com>
2015-07-19 17:53:25 +0545

    JavaFileIO
    An example which makes use of two classes FileInputStream and
    FileOutputStream to copy an input file into an output file.

---
+++ javaFileIO/src/javaFileIO/JavaFileIO.java
@@ -0,0 +1,36 @@
+package javaFileIO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class JavaFileIO {
+
+	public JavaFileIO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String args[]) throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
+		try {
+			in = new FileInputStream("input.txt");
+			out = new FileOutputStream("output.txt");
+			
+			int c;
+			while((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			
+			if (out !=null) {
+				out.close();
+			}
+		}
+	}
+
+}
